3b1e056bdcd7eba8ba63f36a2ad650ab662f616c
liviatran <tran.livia@berkeley.edu>
2019-06-24 22:33:24 -0700

    complete heat map formation

--- heatmap_working.R
+++ heatmap_working.R
@@ -1,9 +1,10 @@
-###heatmap generation script
-#v0.1
+###Heatmap Generation using Generic Mapping Tools (GMT)
+#v0.2
 #by: Livia Tran 
 
-
-
+#required libraries 
+require(stringr)
+require(data.table)
 ###conversions for latitude and longitude
 #converts coordinates to appropriate enumerations based on cardinal directions -- inserts back
 #into appropriate rows in HMD
@@ -59,19 +60,80 @@ motif_coords$longit<-round(as.numeric(motif_coords$longit), digits=4)
 colnames(motif_coords)<-c("lat", "long", "allele_freq", "pop")
 
 
-############heatmap generation 
-library(gmt)
 
 
-#adds a size column to determine how big to make plot points
-motif_coords$size<-rep("3", nrow(motif_coords))
-motif_coords$color<-NULL
-#rearranges columns to fit gmt format 
-motif_coords<-motif_coords[,c("long", "lat", "size", "pop", "allele_freq")]
 
-#world map generation
-pscoast("-R-180/180/-80/80 -JM6i -B30f5/20f2 -Ggrey -K", "heatmap.ps")
+############HEATMAP GENERATION SCRIPT  
+require(gmt)
+require(DescTools)
 
-#plots population coordinate points onto map 
-psxy(motif_coords, cmd="-J -R -Sdp -W1.5p,black -O -K", file="heatmap.ps")
+#puts motif_coords long, lat, and allele frequency columns into a new variable to be converted
+#into gmt format (xyz file)
+gmt_converted_data<-motif_coords[,c(1,2,3,4)]
+
+#rerranges format 
+gmt_converted_data<-gmt_converted_data[,c("long", "lat", "allele_freq", "pop")]
+
+#calls function to convert R object gmt_converted_data to GMT formatted data 
+r2gmt(gmt_converted_data, "DRB1*26F~28E~30Y.xyz")
+
+#finds blockmean of data 
+#-60 for lower bound lat
+#+80 for upper bound lat
+#I for every 3x3 mean value, as obtained from gmt.sh file
+gmt.system("blockmean DRB1*26F~28E~30Y.xyz -R-180/180/-60/80 -I3 > DRB1*26F~28E~30Y.block")
+
+#grids table using surface 
+gmt.system("surface DRB1*26F~28E~30Y.block -R-180/180/-60/80 -Gmotif.grd -I1.8 -T.7 -Ll0")
+
+#creates basemap with correct motif 
+gmt.system("psbasemap -JM6i -R-180/180/-60/80 -B0:.`echo DRB1*26F~28E~30Y.xyz| sed 's/.xyz//g'`: -K > basemap.ps")
+
+#overlays relevant continents onto basemap 
+gmt.system("pscoast -JM6i -R-180/180/-60/80 -A30000 -B0 -G200 -W0.25p -O -K >> basemap.ps")	
+
+#gets upperbound for allele frequencies
+gmt.system("awk '{print $3}' DRB1*26F~28E~30Y.xyz | sort -r | head -1 > upperbound")
+
+#makes a vector called cpt_interval which contains max allelic information and decile increment needed
+#to form deciles
+#uses readLines to obtain upperbound information from bash, and rounds it to the nearest 0.5 
+#uses readLines to obtain decile needed
+cpt_interval<-c(RoundTo(as.numeric(readLines("upperbound")), 0.5), RoundTo(as.numeric(readLines("upperbound")), 0.5)/10)
+
+#creates a vector called decile_interval, which gives decile increments based on cpt_interval information
+decile_interval<-seq(0, cpt_interval[1], cpt_interval[2])
+
+#writes max allelic frequency (rounded) and increment needed to form deciles 
+write(cpt_interval, "max_cpt")
+
+#writes decile interval without any line breaks
+cat(decile_interval, file="deciles")
+
+#makes custom CPT with increments of max_frequency/10 for deciles
+#specifically calls on max_cpt for max frequency and decile increments 
+gmt.system("makecpt -Cseis -Iz -T0/`awk '{print $1}' max_cpt`/`awk '{print $2}' max_cpt` > decile.cpt")
+
+#adds color scale to basemap based on cpt provided
+gmt.system("psscale -D0.1i/1.1i/2i/0.3i -Cdecile.cpt -L -O -K >> basemap.ps")
+
+#overlays more coastlines with pscoast
+gmt.system("pscoast -JM6i -R-180/180/-60/80 -A100000 -Gc -O -K >> basemap.ps")
+
+#clips/masks map areas with no data table coverage -- radius of influence increased to 900 km 
+gmt.system("psmask DRB1*26F~28E~30Y.xyz -R-180/180/-60/80 -I3 -JM6i -S900k -O -K >> basemap.ps")
+
+#grids .grd file onto map 
+gmt.system("grdimage motif.grd -Cdecile.cpt  -JM6i -R-180/180/-60/80 -O -K >> basemap.ps")
+
+#makes a contour map using a .grd file 
+gmt.system("grdcontour motif.grd -JM6i -Cdecile.cpt -A- -R-180/180/-60/80 -O -K >> basemap.ps") 
+
+#plots longtitude/latitude coordinates onto basemap
+gmt.system("psxy DRB1*26F~28E~30Y.xyz -R-180/180/-60/80 -JM6i -A -G255 -W0.5p -Sc.05 -O -K >> basemap.ps")
+
+#overlays coastlines with pcoast 
+gmt.system("pscoast -JM6i -R-180/180/-60/80 -A10000 -W.5p -O  >> basemap.ps")
+
+
 
